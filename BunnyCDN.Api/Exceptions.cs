083ccd3f967dfea3a0081c238a155df049f0048f
Wraptor <wraptor@wheezie.be>
2019-04-30 17:15:25 +0200

    Added more Bunny exceptions * Added message-only BunnyTokenException * Added BunnyZoneException, BunnyNotFoundException, BunnyUnauthorizedException, BunnyBadRequestException

--- BunnyCDN.Api/Exceptions.cs
+++ BunnyCDN.Api/Exceptions.cs
@@ -4,16 +4,49 @@ namespace BunnyCDN.Api
 {
     public class BunnyException : Exception 
     {
-        public BunnyException(string message) : base(message) { }
+        /// <summary>
+        /// A BunnyException wrapper around the Exception, used to catch all BunnyCDN exceptions.
+        /// </summary>
+        /// <param name="message">Error message for base</param>
+        /// <returns>A BunnyCDN exception</returns>
+        public BunnyException(string message = null) : base(message) { }
     }
-
     public class BunnyTokenException : BunnyException
     {
+        /// <summary>
+        /// Token (if any) causing the error.
+        /// </summary>
+        /// <value>Token-string or null if none</value>
         public string Token { get; private set; }
 
+        /// <summary>
+        /// Token exception used for Token faults.
+        /// </summary>
+        /// <param name="message">Error message for base</param>
+        /// <returns>A BunnyCDN token exception</returns>
+        public BunnyTokenException(string message) : base(message) {}
+        
+        /// <summary>
+        /// Token exception used for Token faults.
+        /// </summary>
+        /// <param name="message">Error message for base</param>
+        /// <param name="token">Token causing the exception</param>
+        /// <returns>A BunnyCDN token exception</returns>
         public BunnyTokenException(string message, string token) : base(message)
         {
             this.Token = token;
         } 
+    }
+    public class BunnyZoneException : BunnyException { }
+    public class BunnyNotFoundException : BunnyException { }
+    public class BunnyUnauthorizedException : BunnyException { }
+    public class BunnyBadRequestException : BunnyException
+    {
+        /// <summary>
+        /// Bad Request exception used for input faults.
+        /// </summary>
+        /// <param name="message">Error message for base</param>
+        /// <returns>A BunnyCDN BadRequest exception</returns>
+        public BunnyBadRequestException(string message) : base(message) {}
     }
 }
