856b44221119c5041b21c96400f1ba63163b8fb6
Wraptor <wraptor@wheezie.be>
2019-04-30 13:15:31 +0200

    Added HttpClient for each AccessKey

--- BunnyCDN.Api/AccessKeys.cs
+++ BunnyCDN.Api/AccessKeys.cs
@@ -1,17 +1,40 @@
 ﻿using System;
+using System.Net.Http;
 
 using BunnyCDN.Api.Internals;
 
 namespace BunnyCDN.Api
 {
-    /// Generic AccessKey containing it's token.
-    public class AccessKey
+    /// <summary>
+    /// A token-key provider for API calls
+    /// </summary>
+    public class AccessKey : IDisposable
     {
         public string Token { get; protected set; }
+        public HttpClient Client { get; protected set; }
 
         public AccessKey(string apiKey)
         {
+            SetToken(apiKey);
+        }
+
+        /// <summary>
+        /// Sets the token for the key provider
+        /// </summary>
+        /// <param name="apiKey">API Token</param>
+        public void SetToken(string apiKey)
+        {
             this.Token = apiKey;
+            
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("AccessKey", this.Token);
+            this.Client = httpClient;
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Token = null;
         }
     }
     /// AccountKey used for Account API calls
