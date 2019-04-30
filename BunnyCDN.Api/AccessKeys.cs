358f1e57edb39e883d5eb3644ff8bacb75381433
Wraptor <wraptor@wheezie.be>
2019-04-30 17:42:06 +0200

    Allowed StorageKey & AccountKey instances without arguments

--- BunnyCDN.Api/AccessKeys.cs
+++ BunnyCDN.Api/AccessKeys.cs
@@ -5,14 +5,28 @@ using BunnyCDN.Api.Internals;
 
 namespace BunnyCDN.Api
 {
-    /// <summary>
-    /// A token-key provider for API calls
-    /// </summary>
     public class AccessKey : IDisposable
     {
+        /// <summary>
+        /// API Token required for successful REST calls
+        /// </summary>
+        /// <value>API Token string</value>
         public string Token { get; protected set; }
+        /// <summary>
+        /// HttpClient configured with Token header.
+        /// </summary>
+        /// <value>Preconfigured HttpClient using the token.</value>
         public HttpClient Client { get; protected set; }
 
+        /// <summary>
+        /// Access token provider instance
+        /// </summary>
+        public AccessKey() { }
+
+        /// <summary>
+        /// Access token provider instance
+        /// </summary>
+        /// <param name="apiKey">Access token</param>
         public AccessKey(string apiKey)
         {
             SetToken(apiKey);
@@ -31,15 +45,27 @@ namespace BunnyCDN.Api
             this.Client = httpClient;
         }
 
+        /// <summary>
+        /// Dispose the AccessKey
+        /// </summary>
         public void Dispose()
         {
             Client.Dispose();
             Token = null;
         }
     }
-    /// AccountKey used for Account API calls
+
     public class AccountKey : AccessKey
     {
+        /// <summary>
+        /// Account Token provider
+        /// </summary>
+        public AccountKey() {}
+
+        /// <summary>
+        /// Account Token provider
+        /// </summary>
+        /// <param name="apiKey">API Token</param>
         public AccountKey(string apiKey) : base(apiKey)
         {
             if (!Regexes.AccountToken.IsMatch(apiKey))
@@ -47,9 +73,18 @@ namespace BunnyCDN.Api
         }
     }
 
-    /// StorageKey used for Storage API calls
     public class StorageKey : AccessKey
     {
+        /// <summary>
+        /// Storage Token provider
+        /// </summary>
+        public StorageKey() { }
+
+        
+        /// <summary>
+        /// Storage Token provider
+        /// </summary>
+        /// <param name="apiKey">API Token</param>
         public StorageKey(string apiKey) : base(apiKey)
         {
             if (!Regexes.StorageToken.IsMatch(apiKey))
