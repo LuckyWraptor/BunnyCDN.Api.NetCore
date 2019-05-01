dd098e596df7b5e3312d82dc0e031827299c78fa
Wraptor <wraptor@wheezie.be>
2019-05-01 16:18:03 +0200

    Added variables class containing Storage API url

---
+++ BunnyCDN.Api/Internals/Variables.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BunnyCDN.Api.Internals
+{
+    internal static class Variables
+    {
+        internal const string StorageUrl = "https://storage.bunnycdn.com/"; 
+    }
+}
