1a6b261cd0b05cdadccb1540d8c17502a85049b0
Wraptor <wraptor@wheezie.be>
2019-05-01 19:06:31 +0200

    Added generic MockHandler creation in new MockTools class

---
+++ BunnyCDN.Api.Tests/MockTools.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Xunit;
+
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+
+namespace BunnyCDN.Api.Tests
+{
+    public class MockTools
+    {
+        public static MockHttpMessageHandler GetNewMockHandler()
+        {
+            MockHttpMessageHandler mockHttp = new MockHttpMessageHandler();
+            mockHttp.When("*/throwunauthorized*").Respond(HttpStatusCode.Unauthorized);
+            mockHttp.When("*/throwbadrequest*").Respond(HttpStatusCode.BadRequest, "application/json", JsonConvert.SerializeObject(BadRequestError));
+            mockHttp.When("*/throwinvalidbadrequest*").Respond(HttpStatusCode.BadRequest, "application/json", "");
+            mockHttp.When("*/throwinvalidbadrequest1*").Respond(HttpStatusCode.BadRequest, "application/json", "{}");
+            mockHttp.When("*/thrownotfound*").Respond(HttpStatusCode.NotFound);
+
+            return mockHttp;
+        }
+
+
+        internal static readonly ErrorMessage BadRequestError = new ErrorMessage() { HttpCode = 400, Message = "Specific error message." };
+    }
+}
