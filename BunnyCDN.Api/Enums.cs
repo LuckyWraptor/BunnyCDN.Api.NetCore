90c1387d5fdedaaa91ebb227da4190dc7d986b9b
Wraptor <wraptor@wheezie.be>
2019-05-01 18:38:49 +0200

    Added BillingType enum

---
+++ BunnyCDN.Api/Enums.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BunnyCDN.Api
+{
+    public enum BillingType
+    {
+        Bitcoin = 1,
+        CreditCard = 2,
+        MonthlyUsage = 3,
+        CouponCode = 5
+    }
+}
