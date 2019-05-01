cbffaeae9b23ce0b516dabfca4b54768e0e81c00
Wraptor <wraptor@wheezie.be>
2019-05-01 20:31:28 +0200

    Added missing comments for all current BunnyCDN object classes

--- BunnyCDN.Api/Objects.cs
+++ BunnyCDN.Api/Objects.cs
@@ -4,29 +4,77 @@ namespace BunnyCDN.Api
 {
     public class BillingSummary
     {
+        /// <summary>
+        /// The current account balance
+        /// </summary>
         public double Balance { get; set; }
+        /// <summary>
+        /// The charges for the current month
+        /// </summary>
         public double ThisMonthCharges { get; set; }
+        /// <summary>
+        /// The billing backlog
+        /// </summary>
         public BillingRecord[] BillingRecords { get; set; }
+        /// <summary>
+        /// Current storage price for this month
+        /// </summary>
         public double MonthlyChargesStorage { get; set; }
+        /// <summary>
+        /// Current traffic price for the European geozone
+        /// </summary>
         public double MonthlyChargesEUTraffic { get; set; }
+        /// <summary>
+        /// Current traffic price for the Nort-American geozone
+        /// </summary>
         public double MonthlyChargesUSTraffic { get; set; }
+        /// <summary>
+        /// Current traffic price for the Asian geozone
+        /// </summary>
         public double MonthlyChargesASIATraffic { get; set; }
+        /// <summary>
+        /// Current traffic price for the South-African geozone
+        /// </summary>
         public double MonthlyChargesSATraffic { get; set; }
     }
     public class BillingRecord
     {
+        /// <summary>
+        /// The unique ID of the billing record
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// The amount referenced by the billing record
+        /// </summary>
         public double Amount { get; set; }
+        /// <summary>
+        /// The identifier of the payer who paid for the billing.
+        /// </summary>
         public string Payer { get; set; }
+        /// <summary>
+        /// The unique ID of the payment that the record was created for
+        /// </summary>
         public string PaymentId { get; set; }
+        /// <summary>
+        /// The timestamp when the billing record was created
+        /// </summary>
         public DateTime Timestamp { get { return utcTimestamp; } set { utcTimestamp = DateTime.SpecifyKind(value, DateTimeKind.Utc); } }
         private DateTime utcTimestamp;
+        /// <summary>
+        /// True if there is an invoice available for download for this record
+        /// </summary>
         public bool InvoiceAvailable { get; set; }
+        /// <summary>
+        /// The type of the billing record. *Bitcoin* = 1, *CreditCard* = 2, *MonthlyUsage* = 3, *CouponCode* = 5
+        /// </summary>
         public BillingType Type { get; set; }
     }
 
     public class StorageEntry
     {
+        /// <summary>
+        /// Use-case undocumented
+        /// </summary>
         public long FailIndex { get; set; }
         /// <summary>
         /// Storage object identification number
