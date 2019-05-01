77e88a4c8f66b887f14a4e3f319c1c586434e4c2
Wraptor <wraptor@wheezie.be>
2019-05-01 16:38:44 +0200

    Added Base-API Url to the Variables class

--- BunnyCDN.Api/Internals/Variables.cs
+++ BunnyCDN.Api/Internals/Variables.cs
@@ -4,6 +4,7 @@ namespace BunnyCDN.Api.Internals
 {
     internal static class Variables
     {
-        internal const string StorageUrl = "https://storage.bunnycdn.com/"; 
+        internal const string BaseApiUrl = "https://bunnycdn.com/api/";
+        internal const string StorageUrl = "https://storage.bunnycdn.com/";
     }
 }
