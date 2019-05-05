f82914e9695a4fa00ca984cc51956fb77e758dc6
Wraptor <wraptor@wheezie.be>
2019-05-05 20:46:42 +0200

    Added StorageName Regex and made all regexes match exact

--- BunnyCDN.Api/Internals/Regexes.cs
+++ BunnyCDN.Api/Internals/Regexes.cs
@@ -5,7 +5,8 @@ namespace BunnyCDN.Api.Internals
 {
     internal static class Regexes
     {
-        internal static readonly Regex AccountToken = new Regex(@"([0-9a-fA-F]{8})(-[0-9a-fA-F]{4}){3}-([0-9a-fA-F]{20})(-[0-9a-fA-F]{4}){3}-([0-9a-fA-F]{12})$");
-        internal static readonly Regex StorageToken = new Regex(@"([0-9a-fA-F]{8})(-[0-9a-fA-F]{4}){2}-([0-9a-fA-F]{12})(-[0-9a-fA-F]{4}){2}$");
+        internal static readonly Regex AccountToken = new Regex(@"^([0-9a-fA-F]{8})(-[0-9a-fA-F]{4}){3}-([0-9a-fA-F]{20})(-[0-9a-fA-F]{4}){3}-([0-9a-fA-F]{12})$");
+        internal static readonly Regex StorageToken = new Regex(@"^([0-9a-fA-F]{8})(-[0-9a-fA-F]{4}){2}-([0-9a-fA-F]{12})(-[0-9a-fA-F]{4}){2}$");
+        internal static readonly Regex StorageName = new Regex(@"^([-a-zA-Z0-9]){3,20}$");
     }
 }
