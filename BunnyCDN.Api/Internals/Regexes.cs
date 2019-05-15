3065c460160315eae788e58d1f96747e0dd97ff9
Wraptor <wraptor@wheezie.be>
2019-05-15 18:25:47 +0200

    Added Base64String regex

--- BunnyCDN.Api/Internals/Regexes.cs
+++ BunnyCDN.Api/Internals/Regexes.cs
@@ -9,5 +9,6 @@ namespace BunnyCDN.Api.Internals
         internal static readonly Regex StorageToken = new Regex(@"^([0-9a-fA-F]{8})(-[0-9a-fA-F]{4}){2}-([0-9a-fA-F]{12})(-[0-9a-fA-F]{4}){2}$");
         internal static readonly Regex StorageName = new Regex(@"^([-a-zA-Z0-9]){3,20}$");
         internal static readonly Regex PullZoneName = new Regex(@"^([a-zA-Z0-9]){3,20}$");
+        internal static readonly Regex Base64String = new Regex(@"^[a-zA-Z0-9\+/]*={0,2}$");
     }
 }
